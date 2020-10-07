ec93fa7230093b9422058491e337ca812e0c6ac7
delfin4ik <alexandrsan@ukr.net>
2020-10-07 19:26:34 +0300

    Updates

---
+++ precache-manifest.6ef792a87b33b8eaa83ada958602dbc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bda35dd83393d088208281698ba7a0d2",
+    "url": "/my-chat/index.html"
+  },
+  {
+    "revision": "0a0ed09add86502859fa",
+    "url": "/my-chat/static/css/main.ffec007a.chunk.css"
+  },
+  {
+    "revision": "976c8ae18baf2b46eff6",
+    "url": "/my-chat/static/js/2.fe5bf4f6.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/my-chat/static/js/2.fe5bf4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0ed09add86502859fa",
+    "url": "/my-chat/static/js/main.7381a144.chunk.js"
+  },
+  {
+    "revision": "11b5b06b84e31dd4781a",
+    "url": "/my-chat/static/js/runtime-main.42f38035.js"
+  }
+]);
